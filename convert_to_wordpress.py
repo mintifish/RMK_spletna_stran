ea69458937a04ac184be509a3cfbb3a0fadd0af7
Andrej Sušnik <susnik.andrej7@gmail.com>
2025-10-30 15:38:17 +0100

    Finish publish

--- convert_to_wordpress.py
+++ convert_to_wordpress.py
@@ -254,7 +254,7 @@ Theme Name: RMK Theme
 Theme URI: http://example.com/
 Author: Tian Hrovat  & Andrej Sušnik
 Description: Converted from static site
-Version: 1.2.1
+Version: 1.3.0
 */
 
 /* Add your theme CSS below or edit assets/*.css files copied from the site */
